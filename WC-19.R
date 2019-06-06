294a10e6260c36851d98fc28c82b9dffbd0e40bd
Rutvij <rutvij.bhutaiya@gmail.com>
2019-06-06 17:31:57 +0530

    6/6/19

--- WC-19.R
+++ WC-19.R
@@ -1,8 +1,25 @@
 
 ## ** CRICKET WORLD CUP 2019 ** ## 
 
+## RF Modeling ##
+
+library(randomForest)
+
 wc <- read.csv('WC_Train.csv')
+test1 = read.csv('Test_1992.csv')
 
 # Clusters for WC as test and Previous 4 years Matches
 
 C1992 = wc[which(wc$Year <= 1992),]
+attach(C1992)
+
+summary(C1992)
+
+
+
+
+
+
+
+
+
